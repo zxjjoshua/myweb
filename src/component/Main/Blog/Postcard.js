52fb6d6e479cc242cc8ba7e27cb4efd67e68d1b2
Joshua <153996013@qq.com>
2020-10-25 22:08:52 -0500

    blog delete complete

--- src/component/Main/Blog/Postcard.js
+++ src/component/Main/Blog/Postcard.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import {Link} from 'react-router-dom';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton';
+import InfoDialog from '../../common/InfoDialog';
+import {Redirect} from 'react-router-dom';
 
 const useStyles = makeStyles({
   root: {
@@ -21,27 +26,97 @@ const useStyles = makeStyles({
     textDecoration:'none',
     color:'inherit',
   },
+  blogOps:{
+    float:"right",
+  },
+  hidden:{
+    visibility:"hidden",
+  }
 });
 
 export default function Postcard(props) {
+  const [hovered, setHovered]=useState(false);
   const classes = useStyles();
   const post = props.post;
+
+  function handleMouseHover(e){
+    setHovered(true);
+  }
+
+  function handleMouseLeave(e){
+    setHovered(false);
+  }
+
+  function handleCloseInfoModal(){
+    setMakesure(false);
+  }
+
+  async function handleOnDeletePost(e){
+    console.log(e,"------onDelete");
+    console.log(post, "-------post");
+    setMakesure(true);
+
+  }
+
+  async function deleteArticleRequest(){
+    const data = new FormData();
+    data.append("userid", post.author);
+    data.append("id", post.id);
+    data.append("title", post.title);
+
+    let url="http://localhost:3333/api/blogs/deleteblog/";
+
+    const resp=await fetch(url, {
+      method:"POST",
+      'Access-Control-Allow-Origin':'*',
+      headers:{
+        'Accept': 'application/json',
+      },
+      body:data,
+    })
+    .then(resp=>{return resp.json()});
+    console.log(resp);
+    if(resp.isSucc){
+      handleCloseInfoModal()
+      window.location.reload(false)
+    }else{
+      window.location.reload(false)
+    }
+  }
+
+  const [MakesureDialog, setMakesure]=useState(false);
+
   return (
-    
     <div>
-    <Card className={classes.root}>
-      <Link to={post.url} className={classes.link}>
-      <CardActionArea>
-        <CardContent className={classes.content}>
-          <Typography gutterBottom variant="h5" component="h2">
-            {post.title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {post.brief}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+   <Redirect to="/Blog/bloglist/?offset=0"/>
+    <InfoDialog 
+      isOpen={MakesureDialog} 
+      InfoTitle={"You Are Trying To Delete Your Post"+post.title}
+      clickYesCallback={deleteArticleRequest} 
+      clickNoCallback={handleCloseInfoModal}
+      />
+    <Card className={classes.root} onMouseOver={handleMouseHover} onMouseLeave={handleMouseLeave}>
+    <Link to={post.url} className={classes.link}>
+        <CardActionArea>
+            <CardContent className={classes.content}>
+              <Typography gutterBottom variant="h5" component="h2">
+                {post.title}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {post.brief}
+              </Typography>
+            </CardContent>
+        </CardActionArea>
       </Link>
+      <div className={hovered?classes.blogOps:classes.hidden}>
+          <IconButton aria-label="edit">
+            <EditIcon className={classes.edit}/>
+          </IconButton>
+          <IconButton aria-label="delete" onClick={handleOnDeletePost}>
+            <DeleteIcon className={classes.delete} />
+          </IconButton>
+        </div>
+      
     </Card>
     </div>
   );
--- src/component/common/InfoDialog.js
+++ src/component/common/InfoDialog.js
@@ -5,42 +5,69 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import PropTypes from 'prop-types';
 
 export default function InfoDialog(props) {
-  let [state, setState] = React.useState({
+  const [state, setState] = React.useState({
       open:props.isOpen
   });
 
-  const handleClickOpen = () => {
+  function handleClickOpen(){
     setState({open:true});
   };
 
-  const handleClose = () => {
+  const handleClose=()=>{
     setState({open:false});
   };
 
   console.log(state.open+" and "+props.isOpen+"---- InfoDialog now")
+  let clickYesCallback=handleClose;
+  let clickNoCallback=handleClose;
+  let YesButton="YES"
+  let NoButton="NO"
+  let InfoTitle="You Are Trying To Trigger A Event"
+  let InfoMsg="Are You Sure To Do This?"
+
+  console.log(props, "------InfoDialog")
+  if(props.clickYesCallback!=undefined){
+    clickYesCallback=props.clickYesCallback
+  }
+  if (props.clickNoCallback!=undefined){
+    clickNoCallback=props.clickNoCallback
+  }
+  if (props.YesButton!=undefined){
+    YesButton=props.YesButton
+  }
+  if(props.NoButton!=undefined){
+    NoButton=props.NoButton
+  }
+  if(props.InfoTitle!=undefined){
+    InfoTitle=props.InfoTitle
+  }
+  if(props.InfoMsg!=undefined){
+    InfoMsg=props.InfoMsg
+  }
+
   return (
     <div>
       <Dialog
         open={props.isOpen}
-        onClose={handleClose}
+        onClose={props.handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogTitle id="alert-dialog-title">{InfoTitle}</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous location data to
-            Google, even when no apps are running.
+            {InfoMsg}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Disagree
+          <Button onClick={clickNoCallback} color="primary">
+            {NoButton}
           </Button>
-          <Button onClick={handleClose} color="primary" autoFocus>
-            Agree
+          <Button onClick={clickYesCallback} color="primary" autoFocus>
+            {YesButton}
           </Button>
         </DialogActions>
       </Dialog>
